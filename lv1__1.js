7c9aa6caaa964cc422c4de2ab4c174cb3d219978
dikum98 <dikum98@gmail.com>
2022-02-08 05:33:58 +0900

    doc: 문자열 내 p와 y의 개수

--- lv1__1.js
+++ lv1__1.js
@@ -1,11 +1,13 @@
-// 1. filter 사용 -> 0.1ms
+// 프로그래머스 Lv1. 문자열 내 p와 y의 개수
+
+// 1. filter 사용 => 0.1ms
 function solution(s) {
   const pCount = s.split("").filter((v) => v === "p" || v === "P").length;
   const yCount = s.split("").filter((v) => v === "y" || v === "Y").length;
   return pCount === yCount;
 }
 
-// 2. map 사용 -> 3~4ms
+// 2. map + if문 사용 => 3~4ms
 function solution(s) {
   let pCount = 0;
   let yCount = 0;
@@ -17,7 +19,7 @@ function solution(s) {
   return pCount === yCount;
 }
 
-// 3. for문 사용 -> filter보다 조금 더 빠름
+// 3. for문 + if문 사용 => filter보다 조금 더 빠름
 function solution(s) {
   let pCount = 0;
   let yCount = 0;
