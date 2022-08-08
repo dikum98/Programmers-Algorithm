95453440a7b11ed4bd923f1327a5b7bf7bca6932
dikum98 <dikum98@gmail.com>
2022-08-08 13:53:02 +0900

    정렬 > K번째 수

--- lv1__5.js
+++ lv1__5.js
@@ -9,3 +9,10 @@ function solution(array, commands) {
   }
   return answer;
 }
+
+function solution(array, commands) {
+  return commands.reduce((acc, cur) => {
+    acc.push(array.slice(cur[0] - 1, cur[1]).sort((a, b) => a - b)[cur[2] - 1]);
+    return acc;
+  }, []);
+}
